636514e236760f8efc056a2d96fd3697c8664664
Sachin <saxenas964@gmail.com>
2021-03-23 13:12:52 +0530

    add swagger

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var movieRouter = require('./routes/movie');
@@ -13,14 +16,50 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var breaker=require('express-circuit-breaker');
+
+var CB = breaker({
+    catchError: e => 'trip',
+    handleBlockedRequest: (req, res) => res.sendStatus(500)
+  })
+   
+app.get('/unprotected', (req, res) => res.sendStatus(200))
+   
+app.get('/protected', CB, (req, res) => res.sendStatus(200))
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info:{
+      version:"1.0.0",
+      title:"TMDB API",
+      descripton:"Retrives movies after applying differnet filters",
+      contact: {
+        name :"OGDeveloper"
+      },
+      servers:["http://localhost:3000"]
+    }
+  },
+  apis:["app.js"],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * @swagger
+ * /movie:
+ *  get:
+ *    description: Use to request all movies
+ *    responses:
+ *      200:
+ *        description: A successful response
+ */
 app.use('/movie',movieRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/movie.js
+++ routes/movie.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var service = require('../service/movieservice');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 /* GET movies listing. */
 router.get('/', function (req, res, next) {
         service.getMovie(req,res);
