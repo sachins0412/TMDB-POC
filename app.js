aa2dde2c23248abd56c030146791513d859d81a0
sa20184608 <sachin.saxena7@wipro.com>
2021-03-24 12:26:10 +0530

    configure swagger for headers

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require("swagger-ui-express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var movieRouter = require('./routes/movie');
 var app = express();
 
@@ -16,34 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-var breaker=require('express-circuit-breaker');
 
-var CB = breaker({
-    catchError: e => 'trip',
-    handleBlockedRequest: (req, res) => res.sendStatus(500)
-  })
-   
-app.get('/unprotected', (req, res) => res.sendStatus(200))
-   
-app.get('/protected', CB, (req, res) => res.sendStatus(200))
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info:{
-      version:"1.0.0",
-      title:"TMDB API",
-      descripton:"Retrives movies after applying differnet filters",
-      contact: {
-        name :"OGDeveloper"
-      },
-      servers:["http://localhost:3000"]
-    }
-  },
-  apis:["app.js"],
-};
-
-const swaggerDocs=swaggerJsDoc(swaggerOptions);
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -51,15 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * @swagger
- * /movie:
- *  get:
- *    description: Use to request all movies
- *    responses:
- *      200:
- *        description: A successful response
- */
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 app.use('/movie',movieRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
